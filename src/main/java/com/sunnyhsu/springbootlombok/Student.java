32de347e5fce790f8f6b2c44d21fc044db9f50f1
Chtistina326215 <christina326215@gmail.com>
2025-04-14 16:32:24 +0800

    Lombok

---
+++ src/main/java/com/sunnyhsu/springbootlombok/Student.java
@@ -0,0 +1,13 @@
+package com.sunnyhsu.springbootlombok;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class Student {
+
+    Integer id;
+    String name;
+}
+
